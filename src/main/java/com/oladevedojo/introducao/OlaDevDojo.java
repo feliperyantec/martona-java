9b870d55953c2952c4d6e34f2d138efe44e41df8
F.Ryan <felipe.ryan.tec@gmail.com>
2022-08-20 20:36:11 -0300

    [Aula-09] Comentários

--- src/main/java/com/oladevedojo/introducao/OlaDevDojo.java
+++ src/main/java/com/oladevedojo/introducao/OlaDevDojo.java
@@ -1,6 +1,15 @@
 package com.oladevedojo.introducao;
 
+/**
+ * isso é um comentario <b>javadoc</b><b/>
+ */
 public class OlaDevDojo {
+    // isso é um comentario de uma linha
+    /*
+    isso é um comentario
+    de multiplas
+    linhas
+     */
     public static void main(String[] args) {
         System.out.println("KA-ME-HA-HAAA");
     }
