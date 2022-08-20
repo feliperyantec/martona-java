265332d26301e90f9dc9c67eebbd34fdae163f11
F.Ryan <felipe.ryan.tec@gmail.com>
2022-08-20 20:56:21 -0300

    [Aula-10] int idade = 10;

--- src/main/java/com/oladevedojo/introducao/Aula02TiposPrimitivos.java
+++ src/main/java/com/oladevedojo/introducao/Aula02TiposPrimitivos.java
@@ -3,5 +3,7 @@ package com.oladevedojo.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean SEMPRE MINUSCULAS
+        int idade = 10;
+        System.out.println(idade);
     }
 }
