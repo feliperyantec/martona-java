d0b897df890e505a21af30c43d6b74891d1ce37f
F.Ryan <felipe.ryan.tec@gmail.com>
2022-08-20 21:23:04 -0300

    [Aula-11] 11 - Tipos primitivos pt 02 - declaração e tamanho em memória

--- src/main/java/com/oladevedojo/introducao/Aula02TiposPrimitivos.java
+++ src/main/java/com/oladevedojo/introducao/Aula02TiposPrimitivos.java
@@ -4,7 +4,19 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean SEMPRE MINUSCULAS
         int idade = 10; // variaveis // primeira palavra com inicial minusculas restante maiuscula
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 127;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 59;
+
         System.out.println(idade); // sem concatenação
         System.out.println("A idade é "+idade+" anos"); // com concatenação
+        System.out.println(falso); // com concatenação
+        System.out.println("char "+caractere);
+        // CRTL+D duplicar linha
     }
 }
