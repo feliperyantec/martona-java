27079cdd2422acbe980b1c89d2d38a8d6f8d76fb
F.Ryan <felipe.ryan.tec@gmail.com>
2022-08-20 22:16:36 -0300

    [Aula-14]

---
+++ src/main/java/com/oladevedojo/introducao/Aula03TiposPrimitivosExercicios.java
@@ -0,0 +1,13 @@
+package com.oladevedojo.introducao;
+
+public class Aula03TiposPrimitivosExercicios {
+    public static void main(String[] args) {
+        String nome = "Goku";
+        String endereço = "Rua Russeal, St Universitaio";
+        double salario = 5000;
+        String data = "20/08/2022";
+        String relatorio = "Eu "+nome+" morando no endereço "+endereço+" confirmo que recebi o salario de "+salario+" na data "+data;
+
+        System.out.println(relatorio);
+    }
+}
