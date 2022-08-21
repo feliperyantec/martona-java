5d366251ec6e1af10e210beda64ebfc019c28bb1
F.Ryan <felipe.ryan.tec@gmail.com>
2022-08-21 02:10:46 -0300

    [Aula-24] estruturas condicionais - resolucao de exercicios

--- src/main/java/com/oladevedojo/introducao/Aula05EstruturasCondicionaisExercicio.java
+++ src/main/java/com/oladevedojo/introducao/Aula05EstruturasCondicionaisExercicio.java
@@ -3,7 +3,7 @@ package com.oladevedojo.introducao;
 public class Aula05EstruturasCondicionaisExercicio {
     public static void main(String[] args) {
         //
-        int salario = 444000;
+        int salario = 70000;
         String taxa;
 
         if (salario <= 34712) {
@@ -18,12 +18,19 @@ public class Aula05EstruturasCondicionaisExercicio {
 
         System.out.println(taxa);
 
-        double porcentagem = 49.50;
-        double salario1 = 852;
-
-        double resultado = porcentagem % salario1;
-
-        System.out.println(resultado);
+        //double salarioAnual = 70000;
+        double primeiraFaixa = 9.70 / 100;
+        double segundaFaixa = 37.35 / 100;
+        double terceiraFaixa = 49.50 / 100;
+        double valorImposto;
+        if (salario <= 34712) {
+            valorImposto = salario * primeiraFaixa;
+        } else if (salario >= 34713 && salario <= 68507) {
+            valorImposto = salario * segundaFaixa;
+        } else {
+            valorImposto = salario * terceiraFaixa;
+        }
+        System.out.println(valorImposto);
     }
 
 
---
+++ src/main/java/com/oladevedojo/introducao/Aula05EstruturasCondicionaisResolucaoDoExercicio.java
@@ -0,0 +1,7 @@
+package com.oladevedojo.introducao;
+
+public class Aula05EstruturasCondicionaisResolucaoDoExercicio {
+    public static void main(String[] args) {
+
+    }
+}
