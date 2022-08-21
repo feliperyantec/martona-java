791a48b05b6b8ab9ae5e867c4f8feea553133183
F.Ryan <felipe.ryan.tec@gmail.com>
2022-08-20 23:35:45 -0300

    [Aula-19] operadores atribuicao

---
+++ src/main/java/com/oladevedojo/introducao/Aula04OperadoresAtribuicao.java
@@ -0,0 +1,14 @@
+package com.oladevedojo.introducao;
+
+public class Aula04OperadoresAtribuicao {
+    public static void main(String[] args) {
+        // = += -= *= /= %=
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println(bonus);
+    }
+}
