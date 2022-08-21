c9b84f3b178a3b024bbb4e907f041fbdf1b9ee79
F.Ryan <felipe.ryan.tec@gmail.com>
2022-08-21 00:57:29 -0300

    [Aula-23] estruturas condicionais - operador ternario

---
+++ src/main/java/com/oladevedojo/introducao/Aula05EstruturasCondicionaisoperadorTernario.java
@@ -0,0 +1,22 @@
+package com.oladevedojo.introducao;
+
+public class Aula05EstruturasCondicionaisoperadorTernario {
+    public static void main(String[] args) {
+        // Doar se salario > 5000
+
+        double salario = 7000;
+        String mensagemDoar = "Eu vou doar 500 pro DevDojo";
+        String mensagemNaoDoar = "Ainda não tenho condições, mas vou ter!";
+        String resultado;
+        if (salario > 5000) {
+            resultado = mensagemDoar;
+        }else {
+            resultado = mensagemNaoDoar;
+        }
+        System.out.println(resultado);
+
+        // outro metodo
+        // String resultado = (condicao) ? verdadeiro : falso;
+        String resultado = salario > 5000 ? mensagemDoar : mensagemNaoDoar;
+    }
+}
