bc7d2f89bd86e74020129d11bafc0a595011a705
F.Ryan <felipe.ryan.tec@gmail.com>
2022-08-20 22:34:39 -0300

    [Aula-15]  Operadores - Aritiméticos

---
+++ src/main/java/com/oladevedojo/introducao/Aula04OperadoresAritimeticos.java
@@ -0,0 +1,18 @@
+package com.oladevedojo.introducao;
+
+public class Aula04OperadoresAritimeticos {
+    public static void main(String[] args) {
+        // + - / *
+        // duas variaveis inteiras vão retornar numeros inteiros
+        int numero01 = 10;
+        int numero02 = 20;
+        double resultado = numero01 / numero02;
+        System.out.println(resultado);
+
+        // variaveis diferentes retornam numeros decimais
+        int numero03 = 40;
+        double numero04 = 50;
+        double resultado2 = numero03 / numero04;
+        System.out.println(resultado2);
+    }
+}
