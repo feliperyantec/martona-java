2dfc364ab7a0cff7d8fa1683f611cab5541fc1e7
F.Ryan <felipe.ryan.tec@gmail.com>
2022-08-20 21:05:05 -0300

    [Aula-10] concatenação | variaveis

--- src/main/java/com/oladevedojo/introducao/Aula02TiposPrimitivos.java
+++ src/main/java/com/oladevedojo/introducao/Aula02TiposPrimitivos.java
@@ -3,7 +3,8 @@ package com.oladevedojo.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean SEMPRE MINUSCULAS
-        int idade = 10;
-        System.out.println(idade);
+        int idade = 10; // variaveis // primeira palavra com inicial minusculas restante maiuscula
+        System.out.println(idade); // sem concatenação
+        System.out.println("A idade é "+idade+" anos"); // com concatenação
     }
 }
