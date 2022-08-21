84f32957560554418b03282b895f4c23992251a1
F.Ryan <felipe.ryan.tec@gmail.com>
2022-08-20 23:07:04 -0300

    [Aula-17] operadores logisco AND OR !

---
+++ src/main/java/com/oladevedojo/introducao/Aula04OperadoresLogicosAND.java
@@ -0,0 +1,13 @@
+package com.oladevedojo.introducao;
+
+public class Aula04OperadoresLogicosAND {
+    public static void main(String[] args) {
+        // && (AND) || (or) ! (negação)
+        int idade = 29;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+        System.out.println("isDentroDaLeiMaiorQueTrinta "+isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta "+isDentroDaLeiMenorQueTrinta);
+    }
+}
