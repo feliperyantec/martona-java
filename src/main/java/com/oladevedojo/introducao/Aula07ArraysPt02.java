13f184bc840cc5a3ede05c5562ecaf807dffd0c3
F.Ryan <felipe.ryan.tec@gmail.com>
2022-08-21 21:28:45 -0300

    [Aula-33] Arrays pt 02

---
+++ src/main/java/com/oladevedojo/introducao/Aula07ArraysPt02.java
@@ -0,0 +1,15 @@
+package com.oladevedojo.introducao;
+
+public class Aula07ArraysPt02 {
+    public static void main(String[] args) {
+        // byte, short, int, long, float e double 0
+        // char '\u0000' ' '
+        // boolean false
+        // String null
+
+        String[] nomes = new String[3];
+        System.out.println(nomes[0]);
+        System.out.println(nomes[1]);
+        System.out.println(nomes[2]);
+    }
+}
