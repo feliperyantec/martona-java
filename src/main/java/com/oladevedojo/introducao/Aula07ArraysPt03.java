fb44d5cb33e1b4ae4e8b1ef32b7c9c364caa4bc5
F.Ryan <felipe.ryan.tec@gmail.com>
2022-08-21 21:42:51 -0300

    [Aula-34] Arrays pt 03

---
+++ src/main/java/com/oladevedojo/introducao/Aula07ArraysPt03.java
@@ -0,0 +1,16 @@
+package com.oladevedojo.introducao;
+
+public class Aula07ArraysPt03 {
+    public static void main(String[] args) {
+
+        String[] nomes = new String[4];
+        nomes[0] = "Goku";
+        nomes[1] = "Kurosaki";
+        nomes[2] = "Luffy";
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
+
+    }
+}
