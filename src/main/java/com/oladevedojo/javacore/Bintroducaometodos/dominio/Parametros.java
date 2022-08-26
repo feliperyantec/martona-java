2441a5c3cd13e6d67a4785753f6934b3a20c8fcb
feliperyantec <56183135+feliperyantec@users.noreply.github.com>
2022-08-26 08:08:16 -0300

    [Aula-45] Orientação Objetos - Métodos pt 02 - Parâmetros

---
+++ src/main/java/com/oladevedojo/javacore/Bintroducaometodos/dominio/Parametros.java
@@ -0,0 +1,7 @@
+package com.oladevedojo.javacore.Bintroducaometodos.dominio;
+
+public class Parametros {
+    public void multiplicaDoisNumero(int num1, double num2) {
+        System.out.println(num1 * num2);
+    }
+}
---
+++ src/main/java/com/oladevedojo/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package com.oladevedojo.javacore.Bintroducaometodos.test;
+
+import com.oladevedojo.javacore.Bintroducaometodos.dominio.Parametros;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Parametros parametros = new Parametros();
+        parametros.multiplicaDoisNumero(10, 20.5);
+    }
+}
