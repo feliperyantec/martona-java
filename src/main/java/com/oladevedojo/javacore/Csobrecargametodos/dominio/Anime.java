e45684d8cab335d34629cff0a0145f271f24ec3a
feliperyantec <56183135+feliperyantec@users.noreply.github.com>
2022-09-03 14:35:00 -0300

    [Aula-57] Orientação Objetos - Sobrecarga de métodos

--- src/main/java/com/oladevedojo/javacore/Csobrecargametodos/dominio/Anime.java
+++ src/main/java/com/oladevedojo/javacore/Csobrecargametodos/dominio/Anime.java
@@ -4,11 +4,33 @@ public class Anime {
     private String nome;
     private String tipo;
     private int episodios;
+    private String genero;
+
+    public void init(String nome, String tipo, int episodios){
+        this.nome = nome;
+        this.tipo = tipo;
+        this.episodios = episodios;
+    }
+    public void init(String nome, String tipo, int episodios, String genero){
+        this.init(nome, tipo, episodios);
+        this.genero = genero;
+    }
 
     public void imprime(){
+        System.out.println(this.nome);
         System.out.println(this.tipo);
         System.out.println(this.episodios);
+        System.out.println(this.genero);
     }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
     public void setNome(String nome){
         this.nome = nome;
     }
--- src/main/java/com/oladevedojo/javacore/Csobrecargametodos/test/AnimeTest01.java
+++ src/main/java/com/oladevedojo/javacore/Csobrecargametodos/test/AnimeTest01.java
@@ -5,9 +5,7 @@ import com.oladevedojo.javacore.Csobrecargametodos.dominio.Anime;
 public class AnimeTest01 {
     public static void main(String[] args) {
         Anime anime = new Anime();
-        anime.setNome("Akudama Drive");
-        anime.setTipo("TV");
-        anime.setEpisodios(12);
+        anime.init("Akudama Drive", "TV", 12, "Ação");
         anime.imprime();
     }
 }
