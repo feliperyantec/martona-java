5abc976a1b40400412521d85c15b8da1a71c0d97
feliperyantec <56183135+feliperyantec@users.noreply.github.com>
2022-09-07 18:08:18 -0300

    [Aula-66]Orientação Objetos - Associação pt 03 - Associação unidirecional muitos para um

---
+++ src/main/java/com/oladevedojo/javacore/Gassociacao/test/EscolaTest01.java
@@ -0,0 +1,15 @@
+package com.oladevedojo.javacore.Gassociacao.test;
+
+import com.oladevedojo.javacore.Gassociacao.dominio.Escola;
+import com.oladevedojo.javacore.Gassociacao.dominio.Professor;
+
+public class EscolaTest01 {
+    public static void main(String[] args) {
+        Professor professor1 = new Professor("Jiraya Sensei");
+        Professor professor2 = new Professor("Kakashi Sensei");
+        Professor[] professores = {professor1, professor2};
+        Escola escola = new Escola("Konoha", professores);
+
+        escola.imprime();
+    }
+}
