3cf021cd87cb13807f7f3148ee97dae97bfc2691
feliperyantec <56183135+feliperyantec@users.noreply.github.com>
2022-09-07 16:16:54 -0300

    [Aula-60]Orientação Objetos - Modificador static

--- src/main/java/com/oladevedojo/javacore/Eblocosinicializacao/dominio/Anime.java
+++ src/main/java/com/oladevedojo/javacore/Eblocosinicializacao/dominio/Anime.java
@@ -4,18 +4,24 @@ public class Anime {
     private String nome;
     private int[] episodios;
 
+    {
+        System.out.println("Dentro do bloco de inicialização");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+    }
+
     public Anime(String nome) {
         this.nome = nome;
     }
 
     public Anime() {
-        episodios = new int[100];
-        for (int i = 0; i < episodios.length; i++) {
-            episodios[i] = i+1;
-        }
-        for (int episodio: this.episodios){
+        for (int episodio : this.episodios) {
             System.out.print(episodio + " ");
         }
+        System.out.println();
+
     }
 
     public String getNome() {
@@ -25,4 +31,5 @@ public class Anime {
     public int[] getEpisodios() {
         return episodios;
     }
+
 }
--- src/main/java/com/oladevedojo/javacore/Eblocosinicializacao/test/AnimeTest01.java
+++ src/main/java/com/oladevedojo/javacore/Eblocosinicializacao/test/AnimeTest01.java
@@ -4,6 +4,9 @@ import com.oladevedojo.javacore.Eblocosinicializacao.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime();
+        Anime anime = new Anime("one piece");
+
+        for (int episodio : anime.getEpisodios())
+        System.out.print(episodio + " ");
     }
 }
