1b575a39c8d4eb5248bf90940d64ab1feecdd950
feliperyantec <56183135+feliperyantec@users.noreply.github.com>
2022-09-10 18:02:17 -0300

    [Aula-81]Orientação Objetos - Enumeração pt 02 - Construtores e atributos

--- src/main/java/com/oladevedojo/javacore/Kenumeracao/dominio/TipoCliente.java
+++ src/main/java/com/oladevedojo/javacore/Kenumeracao/dominio/TipoCliente.java
@@ -1,6 +1,23 @@
 package com.oladevedojo.javacore.Kenumeracao.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA(1, "Pessoa Física"),
+    PESSOA_JURIDICA(2, "Pessoa Jurídica");
+
+    private int valor;
+    public final String nomeRelatorio;
+
+
+    TipoCliente(int valor, String nomeRelatorio) {
+        this.valor = valor;
+        this.nomeRelatorio = nomeRelatorio;
+    }
+
+    public String getNomeRelatorio() {
+        return nomeRelatorio;
+    }
+
+    public int getValor() {
+        return valor;
+    }
 }
--- src/main/java/com/oladevedojo/javacore/Kenumeracao/test/ClienteTest01.java
+++ src/main/java/com/oladevedojo/javacore/Kenumeracao/test/ClienteTest01.java
@@ -3,17 +3,15 @@ package com.oladevedojo.javacore.Kenumeracao.test;
 import com.oladevedojo.javacore.Kenumeracao.dominio.Cliente;
 import com.oladevedojo.javacore.Kenumeracao.dominio.TipoCliente;
 
-import java.awt.event.ActionListener;
+import static com.oladevedojo.javacore.Kenumeracao.dominio.Cliente.TipoPagamento.CREDITO;
+import static com.oladevedojo.javacore.Kenumeracao.dominio.Cliente.TipoPagamento.DEBITO;
+
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente3 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente4= new Cliente("Tsubasa", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, DEBITO);
+        Cliente cliente2 = new Cliente("Akira", TipoCliente.PESSOA_JURIDICA, CREDITO);
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
     }
 }
