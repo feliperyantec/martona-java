8075ef7b991be7041b73f1669f56bb17440a97a7
feliperyantec <56183135+feliperyantec@users.noreply.github.com>
2022-09-10 15:43:58 -0300

    [Aula-73]Orientação Objetos - Herança pt 03 - protected

--- src/main/java/com/oladevedojo/javacore/Hheranca/dominio/Funcionario.java
+++ src/main/java/com/oladevedojo/javacore/Hheranca/dominio/Funcionario.java
@@ -3,9 +3,13 @@ package com.oladevedojo.javacore.Hheranca.dominio;
 public class Funcionario extends Pessoa {
     private double salario;
 
-    public void imprime(){
+    public void imprime() {
         super.imprime();
         System.out.println(this.salario);
+    }
+
+    public void relatorioPagamento() {
+        System.out.println("Eu " + this.nome + " Recebi o salario de " + this.salario);
     }
 
     public double getSalario() {
