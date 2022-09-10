1331a13c0830e75247736f5a8f532b6380c2c6da
feliperyantec <56183135+feliperyantec@users.noreply.github.com>
2022-09-10 16:30:42 -0300

    [Aula-76]Orientação Objetos - Sobrescrita do método toString

---
+++ src/main/java/com/oladevedojo/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package com.oladevedojo.javacore.Isobrescrita.test;
+
+import com.oladevedojo.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+
+        System.out.println(anime);
+    }
+}
