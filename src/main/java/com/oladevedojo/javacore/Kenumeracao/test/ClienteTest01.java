94bef07e2c6119e40fcf78b4b6a28f6c7e7d7878
feliperyantec <56183135+feliperyantec@users.noreply.github.com>
2022-09-12 19:05:33 -0300

    [Aula-83]Orientação Objetos - Enumeração pt 04 - Busca por atributos

--- src/main/java/com/oladevedojo/javacore/Kenumeracao/test/ClienteTest01.java
+++ src/main/java/com/oladevedojo/javacore/Kenumeracao/test/ClienteTest01.java
@@ -15,5 +15,9 @@ public class ClienteTest01 {
         System.out.println(cliente1);
         System.out.println(cliente2);
         System.out.println(DEBITO.calcularDesconto(100));
+        TipoCliente tipoCliente = TipoCliente.valueOf("PESSOA_FISICA");
+        System.out.println(tipoCliente.getNomeRelatorio());
+        TipoCliente tipoCliente2 = TipoCliente.tipoClientePorNomeRelatorio("Pessoa Física");
+        System.out.println(tipoCliente2);
     }
 }
