a9fcbc1faea88d92bb979c64b96a0e98052f6897
feliperyantec <56183135+feliperyantec@users.noreply.github.com>
2022-09-13 19:45:17 -0300

    [Aula-91]Orientação Objetos - Polimorfismo pt 02 - Funcionamento

---
+++ src/main/java/com/oladevedojo/javacore/Npolimorfismo/test/ProdutoTest02.java
@@ -0,0 +1,20 @@
+package com.oladevedojo.javacore.Npolimorfismo.test;
+
+import com.oladevedojo.javacore.Npolimorfismo.dominio.Computador;
+import com.oladevedojo.javacore.Npolimorfismo.dominio.Produto;
+import com.oladevedojo.javacore.Npolimorfismo.dominio.Tomate;
+
+public class ProdutoTest02 {
+    public static void main(String[] args) {
+        Produto produto = new Computador("Ryzen 9", 3000);
+        System.out.println(produto.getNome());
+        System.out.println(produto.getValor());
+        System.out.println(produto.calcularImposto());
+        System.out.println("-----------------------");
+
+        Produto produto1 = new Tomate("Americano", 20);
+        System.out.println(produto1.getNome());
+        System.out.println(produto1.getValor());
+        System.out.println(produto1.calcularImposto());
+    }
+}
