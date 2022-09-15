2bacbec0c9ce5bb0b5ef9a8796c75fe3b794dd6a
feliperyantec <56183135+feliperyantec@users.noreply.github.com>
2022-09-15 12:58:49 -0300

    [Aula-102]Exceções pt 08 - Multi catch em linha

--- src/main/java/com/oladevedojo/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
+++ src/main/java/com/oladevedojo/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
@@ -1,19 +1,27 @@
 package com.oladevedojo.javacore.Oexception.runtime.test;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class RuntimeExceptionTest04 {
     public static void main(String[] args) {
         try {
             throw new RuntimeException();
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Dentro do ArrayIndexOutOfBoundsException");
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Dentro do IndexOutOfBoundsException");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Dentro do IndexOutOfBoundsException");
-        } catch (ArithmeticException e) {
-            System.out.println("Dentro do ArithmeticException");
-        }catch (RuntimeException e){
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e) {
+            System.out.println("Dentro do ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException");
+        } catch (RuntimeException e) {
             System.out.println("Dentro do RuntimeException");
         }
+
+        try {
+            talvezLanceException();
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void talvezLanceException() throws SQLException, IOException {
+
     }
 }
