bd5bfcaa9a8d1847da9dd08e1956212277507fe6
feliperyantec <56183135+feliperyantec@users.noreply.github.com>
2022-09-15 09:07:52 -0300

    [Aula-99]Exceções pt 05 - Lançando exceção checked

--- src/main/java/com/oladevedojo/javacore/Oexception/runtime/test/RuntimeExceptionTest02.java
+++ src/main/java/com/oladevedojo/javacore/Oexception/runtime/test/RuntimeExceptionTest02.java
@@ -5,6 +5,14 @@ public class RuntimeExceptionTest02 {
         System.out.println(divisao(1, 0));
     }
 
+    /**
+     *
+     * @param a
+     * @param b não pode ser zero
+     * @return
+     * @throws IllegalArgumentException caso b seja zero
+     */
+
     private static int divisao(int a, int b) {
         if (b == 0){
             throw new IllegalArgumentException("Argumento Invalido, não pode ser 0");
