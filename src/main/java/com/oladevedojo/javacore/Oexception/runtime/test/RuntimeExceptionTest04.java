71a0587e61c9e7c037f485922c92365a73c3611f
feliperyantec <56183135+feliperyantec@users.noreply.github.com>
2022-09-15 12:45:22 -0300

    [Aula-101]Exceções pt 07 - Capturando múltiplas exceções

---
+++ src/main/java/com/oladevedojo/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
@@ -0,0 +1,19 @@
+package com.oladevedojo.javacore.Oexception.runtime.test;
+
+public class RuntimeExceptionTest04 {
+    public static void main(String[] args) {
+        try {
+            throw new RuntimeException();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Dentro do ArrayIndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Dentro do IndexOutOfBoundsException");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Dentro do IndexOutOfBoundsException");
+        } catch (ArithmeticException e) {
+            System.out.println("Dentro do ArithmeticException");
+        }catch (RuntimeException e){
+            System.out.println("Dentro do RuntimeException");
+        }
+    }
+}
