2a148f86c4678eb0f0e614d8d4e381cd49647ec1
feliperyantec <56183135+feliperyantec@users.noreply.github.com>
2022-09-15 09:40:13 -0300

    [Aula-100]Exceções pt 06 - Bloco Finally

---
+++ src/main/java/com/oladevedojo/javacore/Oexception/runtime/test/RuntimeExceptionTest03.java
@@ -0,0 +1,34 @@
+package com.oladevedojo.javacore.Oexception.runtime.test;
+
+public class RuntimeExceptionTest03 {
+    public static void main(String[] args) {
+        abreConexao();
+        abreConexao2();
+    }
+
+    private static String abreConexao() {
+        try {
+            System.out.println("Abrindo arquivo");
+            System.out.println("Escrevendo dados no arquivo");
+            return "conexao aberta";
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Fechando recurso liberado pelo SO");
+            System.out.println("-----------------------");
+        }
+        return null;
+
+    }
+
+
+    private static String abreConexao2() {
+        try {
+            System.out.println("Abrindo arquivo");
+            System.out.println("Escrevendo dados no arquivo");
+            throw new RuntimeException();
+        }finally {
+            System.out.println("Fechando recurso liberado pelo SO");
+        }
+    }
+}
