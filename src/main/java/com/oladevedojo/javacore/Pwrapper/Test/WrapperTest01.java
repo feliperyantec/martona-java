e0b6700691e2257f9b9c69fd1f263e5ba98934c9
feliperyantec <56183135+feliperyantec@users.noreply.github.com>
2022-09-23 20:10:09 -0300

    [Aula-107]Classes Utilitárias - Wrappers pt 02

--- src/main/java/com/oladevedojo/javacore/Pwrapper/Test/WrapperTest01.java
+++ src/main/java/com/oladevedojo/javacore/Pwrapper/Test/WrapperTest01.java
@@ -13,11 +13,23 @@ public class WrapperTest01 {
 
         Byte byteW = 1;
         Short shortW = 1;
-        Integer intW = 1;
+        Integer intW = 1; // autoboxing
         Long longW = 10L;
         Float floatW = 10F;
         Double doubleW = 10D;
         Character charW = 'W';
         Boolean booleanW = false;
+
+        int i = intW; // unboxing
+        Integer intW2 = Integer.parseInt("1");
+        boolean verdadeiro = Boolean.parseBoolean("TruE");
+
+        System.out.println(Character.isDigit('A'));
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isLetterOrDigit('!'));
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.isLowerCase('a'));
+        System.out.println(Character.toUpperCase('a'));
+        System.out.println(Character.toLowerCase('A'));
     }
 }
