9d03c29cb87a86991dd5ee0f5a7b2a81a3cb19aa
feliperyantec <56183135+feliperyantec@users.noreply.github.com>
2022-09-23 19:38:04 -0300

    [Aula-105]Exceções pt 11 - Exceção e regras de sobrescrita

---
+++ src/main/java/com/oladevedojo/javacore/Oexception/exception/dominio/Funcionario.java
@@ -0,0 +1,10 @@
+package com.oladevedojo.javacore.Oexception.exception.dominio;
+
+import java.io.FileNotFoundException;
+
+public class Funcionario extends Pessoa {
+    public void salvar() throws LoginInvalidoException {
+        System.out.println("Salvando pessoa");
+    }
+
+}
---
+++ src/main/java/com/oladevedojo/javacore/Oexception/exception/test/SobrescritaComExceptionTest01.java
@@ -0,0 +1,18 @@
+package com.oladevedojo.javacore.Oexception.exception.test;
+
+import com.oladevedojo.javacore.Oexception.exception.dominio.Funcionario;
+import com.oladevedojo.javacore.Oexception.exception.dominio.LoginInvalidoException;
+import com.oladevedojo.javacore.Oexception.exception.dominio.Pessoa;
+
+public class SobrescritaComExceptionTest01 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        Funcionario funcionario = new Funcionario();
+
+        try {
+            funcionario.salvar();
+        } catch (LoginInvalidoException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
