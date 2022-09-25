d1c6843799f7a1ad8f6303511a5ebae06a2f4fd6
feliperyantec <56183135+feliperyantec@users.noreply.github.com>
2022-09-25 14:25:39 -0300

    [Aula-131]Classes Utilitárias - Regex pt 01 - Pattern e Matcher

---
+++ src/main/java/com/oladevedojo/javacore/Uregex/test/PatternMatcherTest01.java
@@ -0,0 +1,19 @@
+package com.oladevedojo.javacore.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest01 {
+    public static void main(String[] args) {
+        String regex = "aba";
+        String texto = "abababa";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("texto:  "+texto);
+        System.out.println("indice: 0123456789");
+        System.out.println("regex "+regex);
+        while (matcher.find()){
+            System.out.print(matcher.start()+" ");
+        }
+    }
+}
