b57352b335ab0812d64b0d56c6cac61ac5f0ceff
feliperyantec <56183135+feliperyantec@users.noreply.github.com>
2022-09-25 09:47:26 -0300

    [Aula-121]Classes Utilitárias - LocalDateTime

---
+++ src/main/java/com/oladevedojo/javacore/Rdate/Test/LocalDateTimeTest01.java
@@ -0,0 +1,20 @@
+package com.oladevedojo.javacore.Rdate.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class LocalDateTimeTest01 {
+    public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        LocalDate date = LocalDate.parse("2022-08-06");
+        LocalTime time = LocalTime.parse("23:45:04");
+        System.out.println(localDateTime);
+        System.out.println(date);
+        System.out.println(time);
+        LocalDateTime ldt1 = date.atTime(time);
+        LocalDateTime ldt2 = time.atDate(date);
+        System.out.println(ldt1);
+        System.out.println(ldt2);
+    }
+}
