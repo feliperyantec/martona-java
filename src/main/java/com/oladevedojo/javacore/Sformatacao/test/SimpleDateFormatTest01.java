ad99456291ad5952aa8a82c64b967821688ccf0f
feliperyantec <56183135+feliperyantec@users.noreply.github.com>
2022-09-25 09:01:10 -0300

    [Aula-118]Classes Utilitárias - SimpleDateFormat

---
+++ src/main/java/com/oladevedojo/javacore/Sformatacao/test/SimpleDateFormatTest01.java
@@ -0,0 +1,18 @@
+package com.oladevedojo.javacore.Sformatacao.test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleDateFormatTest01 {
+    public static void main(String[] args) {
+        String pattern = "'Brazil' dd 'de' MMMM 'de' yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        System.out.println(sdf.format(new Date()));
+        try {
+            System.out.println(sdf.parse("'Brazil' dd 'de' MMMM 'de' yyyy"));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
